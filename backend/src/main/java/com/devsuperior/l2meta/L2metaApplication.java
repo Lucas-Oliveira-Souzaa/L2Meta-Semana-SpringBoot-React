c32b50e2da4d586a1013d7c125bd748c2f472161
Lucas Oliveira <lucas16gama@gmail.com>
2022-07-11 23:58:26 -0200

    Criação do Projeto

---
+++ backend/src/main/java/com/devsuperior/l2meta/L2metaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.l2meta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L2metaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L2metaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/l2meta/L2metaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.l2meta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L2metaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
