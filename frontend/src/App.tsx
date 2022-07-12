11049e71196eeeeb04ec9aeafb338d4c8aef3048
Lucas Oliveira <lucas16gama@gmail.com>
2022-07-12 00:43:50 -0200

    Primeiro Componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,17 @@
+import NotificaçãoBotão from "./componentes/NotificaçãoBotão"
+
 function App() {
   return(
+    <>    
     <h1>Olá Mundo!</h1>
+    <NotificaçãoBotão/>
+    <NotificaçãoBotão/>
+    <NotificaçãoBotão/>
+    <NotificaçãoBotão/>
+    <NotificaçãoBotão/>
+    <NotificaçãoBotão/>
+    </>
+
   )
 }
 
---
+++ frontend/src/componentes/NotificaçãoBotão/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/notificimagem.png'
+
+import './styles.css'
+
+function NotificaçãoBotão() {
+    return (
+        <div className="l2meta-red-btn">
+            <img src={icon} alt="Notificar"/>
+        </div>
+    )
+}
+
+export default NotificaçãoBotão
