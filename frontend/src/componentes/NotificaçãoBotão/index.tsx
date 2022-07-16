72150db60b78414e992c3d685a7d3ed3c8f6c1d5
Lucas Oliveira <lucas16gama@gmail.com>
2022-07-16 14:24:08 -0200

    Send notification

--- frontend/src/componentes/NotificaçãoBotão/index.tsx
+++ frontend/src/componentes/NotificaçãoBotão/index.tsx
@@ -1,10 +1,23 @@
+import axios from 'axios';
 import icon from '../../assets/img/notificimagem.png'
+import { BASE_URL } from '../../utils/request';
 
 import './styles.css'
 
-function NotificaçãoBotão() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id : number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response =>{
+        console.log("Sucesso");
+    })
+}
+
+function NotificaçãoBotão( {saleId} :Props) {
     return (
-        <div className="l2meta-blue-btn">
+        <div className="l2meta-blue-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar"/>
         </div>
     )
